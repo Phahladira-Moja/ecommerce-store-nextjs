1fa166f6ede5946c0511647f2f4bef7211c3ec4f
Phahladira-Moja <phahladira@patala.co.za>
2023-08-01 21:41:19 +0200

    Added the products to the store
    - Created the get products and billboard actions
    - created the Product card and list component
    - created the currency, billboard , icon button and no result component
    -

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/container";
+import getBillboards from "@/actions/getBillboards";
+import getProducts from "@/actions/getProducts";
+import ProductsList from "@/components/ProductsList";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboards("149b60ad-4cd1-45e5-a677-87874c715c0c");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductsList title="Featured Products" items={products} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
