034700520ecb1e9bdba2d997709d4dae099f18d3
Phahladira-Moja <phahladira@patala.co.za>
2023-08-02 23:18:37 +0200

    Added the Filters and Category ID page
    - Added the mobile filters component
    - created the getCategory, getColors, getSizes actions

---
+++ app/(routes)/category/[categoryId]/components/Filter.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React from "react";
+import qs from "query-string";
+import { cn } from "@/libs/utils";
+
+import { Color, Size } from "@/types";
+import Button from "@/components/ui/Button";
+import { useRouter, useSearchParams } from "next/navigation";
+
+interface FilterProps {
+  data: (Size | Color)[];
+  name: string;
+  valueKey: string;
+}
+
+const Filter: React.FC<FilterProps> = ({ data, name, valueKey }) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const selectedValue = searchParams.get(valueKey);
+
+  const onClick = (id: string) => {
+    const current = qs.parse(searchParams.toString());
+
+    const query = {
+      ...current,
+      [valueKey]: id,
+    };
+
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  };
+
+  return (
+    <div className="mb-8">
+      <h3 className="text-lg font-semibold">{name}</h3>
+      <hr className="my-4" />
+      <div className="flex flex-wrap gap-2">
+        {data.map((filter) => (
+          <div key={filter.id} className="flex items-center">
+            <Button
+              className={cn(
+                "rounded-md text-sm text-gray-800 p-2 bg-white border border-gray-300",
+                selectedValue === filter.id && "bg-black text-white"
+              )}
+              onClick={() => onClick(filter.id)}
+            >
+              {filter.name}
+            </Button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
